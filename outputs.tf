8be96ba6e39262c094eddafdd64b2be8b8cfc450
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-07-09 16:02:22 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@
 output "AmazonLinux_availability_zone" {
   value = aws_instance.myFirst_TF-server.availability_zone
 }
+
+#Terraform console output
+output "Ubuntu_availability_zone" {
+  value = aws_instance.mySecond_TF-server.availability_zone
+}
