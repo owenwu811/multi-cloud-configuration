7c3d7aef25129a9d73653599fa5a0438d91f924a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-07-09 16:08:11 -0700

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,6 +2,46 @@
 #Configure AWS Provider
 #Operational environment is set to run in <us-east-1> region
 
+
+#importing infrastructure into terraform's control
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+
 resource "aws_s3_bucket" "b2" {
   bucket = "thisismybucket"
 
