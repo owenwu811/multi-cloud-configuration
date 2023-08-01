6a42fca5746a678ddfcd730d3920258faca363a7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-01 07:11:43 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,8 @@ output "AmazonLinux_availability_zone" {
 output "Ubuntu_availability_zone" {
   value = aws_instance.mySecond_TF-server.availability_zone
 }
-
+#Terraform console output 
 output "Load_Balancer_DNS" {
     value = aws_elb.my-elb.dns_name
 }
+
