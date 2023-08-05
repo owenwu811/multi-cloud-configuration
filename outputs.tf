38c380168bd2068f5b2cb6d6f07fee7a0bad1512
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-05 16:47:48 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,17 @@
 #Terraform console output
 output "AmazonLinux_availability_zone" {
   value = aws_instance.myFirst_TF-server.availability_zone
+  description = "availablility zone of linux ec2 instance"
 }
 
 #Terraform console output
 output "Ubuntu_availability_zone" {
   value = aws_instance.mySecond_TF-server.availability_zone
+  description = "name of the availbility zone that the ec2 instance resides in"
 }
 #Terraform console output 
 output "Load_Balancer_DNS" {
     value = aws_elb.my-elb.dns_name
+    description = "name of the elastic load balancer"
 }
 
